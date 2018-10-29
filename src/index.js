b81d2a4188fbde4f09e32fd18eed47c71ad4d539
kishorkira <kishorkira@gmail.com>
2018-10-29 17:27:34 +0530

    Add youtube api key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyCD87a-4jfEsaSqPYDR6RXCw5rm2d23bpY";
 const App = () =>{
-    return <div>Hi</div>
+    return (
+        <div>
+            <SearchBar/>
+
+        </div>
+    );
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
