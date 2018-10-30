778ee87a583dc90a51f66f483da702b1f356ff68
kishorkira <kishorkira@gmail.com>
2018-10-30 21:36:48 +0530

    search with axios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+import axios from "axios";
 
 import API_KEY from "./API_KEY";
 import SearchBar from "./components/search_bar";
@@ -18,12 +19,35 @@ class App extends Component {
     this.videoSearch("movie");
   }
   videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
+    axios
+      .get("https://www.googleapis.com/youtube/v3/search", {
+        params: {
+          key: API_KEY,
+          type: "video",
+          part: "snippet",
+          q: term,
+          maxResults: 10
+        }
+      })
+      .then(response => {
+        this.setState({
+          videos: response.data.items,
+          selectedVideo: response.data.items[0]
+        });
       });
-    });
+    // YTSearch(
+    //   {
+    //     key: API_KEY,
+    //     term: term,
+    //     maxResults: 30
+    //   },
+    //   videos => {
+    //     this.setState({
+    //       videos: videos,
+    //       selectedVideo: videos[0]
+    //     });
+    //   }
+    // );
   }
   render() {
     const videoSearch = _.debounce(term => this.videoSearch(term), 600);
@@ -32,7 +56,11 @@ class App extends Component {
         <SearchBar onSerchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          onVideoSelect={selectedVideo =>
+            this.setState({
+              selectedVideo
+            })
+          }
           videos={this.state.videos}
         />
       </div>
